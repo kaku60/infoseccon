7afd6b625cde38452555698ea289d2bda08a052d
kaku60 <kaku60@users.noreply.github.com>
2014-04-15 20:00:33 +0900

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,22 +2,24 @@
 // This is a template for a PHP scraper on Morph (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+
+// Read in a page
+$html = scraperwiki::scrape("http://www.concise-courses.com/security/conferences-of-2014/");
+
+// Find something on the page using css selectors
+$dom = new simple_html_dom();
+$dom->load($html);
+preg_match("/Date: ([0-9]*)<br \/>/i", $html, $date);
+
+print_r($date);
+
+// Write out to the sqlite database using scraperwiki library
+//scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
+
+// An arbitrary query against the database
+//scraperwiki::select("* from data where 'name'='peter'")
 
 // You don't have to do things with the ScraperWiki library. You can use whatever is installed
 // on Morph for PHP (See https://github.com/openaustralia/morph-docker-php) and all that matters
